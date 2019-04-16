7a382231e607fc2a5aea092c2f7199f04e0f87e2
Ruda Maia <rudamaia@gmail.com>
2019-04-16 11:54:59 -0400

    Update tranq.js

--- js/tranq.js
+++ js/tranq.js
@@ -5,29 +5,32 @@ var html5player = ""
 //_gDysWhXtwQ
 //sP6SaKn2WuM"
 iframe.src = "https://www.youtube.com/live_chat?v=_gDysWhXtwQ";
-iframe.className += '.tranq-chatBase'
-addSheet("chat.css", true);
-addSheet("player.css", false);
 iframe.setAttribute("allowTransparency", "true");
 iframe.setAttribute("id", "chat-iframe");
 player[0].appendChild(iframe);
 
-
 iframe.onload = loadedIframe
-var iframeContent = iframe.contentDocument
+var iframeContent
+
+var cssPlayer = getCSS("player.css")
+var cssChat = getCSS("chat.css")
 
 function loadedIframe() {
+  iframeContent = iframe.contentDocument
   iframeContent.getElementsByTagName('html')[0].setAttribute("dark", "");
   // console.log(iframeContent.getElementsByTagName('yt-live-chat-message-renderer'))
   if (iframeContent.querySelector('.live-chat-unavailable') !== null || iframeContent.querySelector('#chat-messages') == null) {
     document.getElementById('chat-iframe').remove();
   }
   else {
+    addSheet(cssChat, true);
+    addSheet(cssPlayer, false);
     html5player = document.getElementsByClassName('html5-video-player')[0];
-    iframe.style.visibility = 'visible'
-    iframe.style.opacity = '1'
+    show();
+
   }
 }
+
 function chatLayout(layout) {
   switch (layout) {
     case "fullscreen":
@@ -37,36 +40,70 @@ function chatLayout(layout) {
         classlist.toggle('tranq-player-right');
       }
       iframe.className = 'tranq-fullscreen'
+      show();
       break;
     case "right":
       chatPosition(false)
       html5player.classList.toggle('tranq-player-right');
       iframe.className = 'tranq-chatright'
+      show(true);
       break;
     case "hide":
       iframe.classList.toggle('tranq-chathidden');
+      show();
       break;
   }
 }
 
 function chatPosition(inside) {
+  hide();
   if(inside) {
     html5player.appendChild(iframe);
   }
   else {
+
     document.getElementById('player').appendChild(iframe)
+    document.getElementById('player').appendChild(html5player)
   }
 }
 
-function addSheet(url, iframe) {
-  var urlBase = "https://raw.githubusercontent.com/rudamaia/tranqStyles/master/css/"
-  var style = document.createElement("link");
-  style.setAttribute("rel", "stylesheet");
-  style.setAttribute("type", "text/css");
-  style.setAttribute("href", urlBase + url);
+function addSheet(css, iframe) {
+  var style = document.createElement("style");
+  style.type = 'text/css';
+  style.setAttribute("name", css[1].split('.')[0]);
+  style.appendChild(document.createTextNode(css[0]));
+
   var target = iframe ? iframeContent : document;
   target.getElementsByTagName("head")[0].appendChild(style)
+  console.log(css[1].split('.')[0])
 }
 
+function getCSS(css) {
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open('GET', "https://raw.githubusercontent.com/rudamaia/tranqStyles/master/css/" + css, false);
+  xmlhttp.send();
+  return [xmlhttp.responseText, css]
+}
+
+function show(chatright) {
+  setTimeout(function() {
+    var ifc = iframeContent.getElementsByTagName('html')[0];
+    iframe.style.visibility = 'visible';
+    iframe.style.opacity = '1';
+    if(chatright) {
+      ifc.setAttribute("class", "tranq-chat-right");
+    }
+    else {
+      if(ifc.getAttribute('class')) {
+        ifc.removeAttribute("class");
+      }
+    }
+  }, 3000);
+
+}
+function hide() {
+  iframe.style.visibility = 'hidden'
+  iframe.style.opacity = '0'
+}
 
 // stringfy the JS https://www.freeformatter.com/javascript-escape.html#ad-output
