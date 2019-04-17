0ccf6f45cc8bde04a2140b9b4415e49c9f752f27
Ruda Maia <rudamaia@gmail.com>
2019-04-16 21:59:48 -0400

    js update

---
+++ js/fullscreen.js
@@ -0,0 +1,20 @@
+var cssChat = getCSS("chat.css")
+addSheet(cssChat)
+
+function addSheet(css) {
+  // initiate vars
+  var style = document.createElement("style");
+
+  style.type = 'text/css';
+  style.setAttribute("name", css[1].split('.')[0]);
+  style.appendChild(document.createTextNode(css[0]));
+
+  document.getElementsByTagName("head")[0].appendChild(style)
+}
+
+function getCSS(css) {
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open('GET', "https://raw.githubusercontent.com/rudamaia/tranqStyles/master/css/" + css, false);
+  xmlhttp.send();
+  return [xmlhttp.responseText, css]
+}
--- js/tranq.js
+++ js/tranq.js
@@ -1,29 +1,9 @@
 document.getElementsByTagName('html')[0].setAttribute("dark", "");
 document.onload = onLoad
 
-var cssChat = getCSS("chat.css")
-
 function onLoad() {
   if (document.querySelector('.live-chat-unavailable') !== null || document.querySelector('#chat-messages') == null) {
     return "chat unavailable'"
   }
-  addSheet(cssChat)
-}
 
-function addSheet(css) {
-  // initiate vars
-  var style = document.createElement("style");
-
-  style.type = 'text/css';
-  style.setAttribute("name", css[1].split('.')[0]);
-  style.appendChild(document.createTextNode(css[0]));
-
-  document.getElementsByTagName("head")[0].appendChild(style)
-}
-
-function getCSS(css) {
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.open('GET', "https://raw.githubusercontent.com/rudamaia/tranqStyles/master/css/" + css, false);
-  xmlhttp.send();
-  return [xmlhttp.responseText, css]
 }
